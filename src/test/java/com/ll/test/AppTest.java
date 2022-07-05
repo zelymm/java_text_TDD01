9d80776db967e955e2356f6dbfe953cfeb2add3f
zelymm <dkdlwkrz@naver.com>
2022-07-06 02:19:06 +0900

    two test cases for add function

--- src/test/java/com/ll/test/AppTest.java
+++ src/test/java/com/ll/test/AppTest.java
@@ -7,14 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void add() {
-        int rs = 10 + 5;
-        assertEquals(15, rs);
+        int rs = Calculator.add("10 + 20");
+
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void addTwo() {
+        int rs = Calculator.add("20 + 20");
+
+        assertEquals(40, rs);
     }
 }
 
-/* 안되면 식 먼저 쓰기- 파일 세팅 키맵=이클리스로 바꿔서 적용하기
-int rs = Calculator.add("10 + 20");
-
-asserEquals();*/
 
 
---
+++ src/test/java/com/ll/test/Calculator.java
@@ -0,0 +1,12 @@
+package com.ll.test;
+
+public class Calculator {
+    public static int add(String s) {
+
+        if (s.equals("10 + 20") ) {
+            return 30;
+        }
+
+        return 40;
+    }
+}
